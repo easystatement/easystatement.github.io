3adfbfa7de2682630811addef493f9aa4c8d12e1
Paolo Boni <paolo.boni@digital.hmrc.gov.uk>
2018-04-13 17:59:19 +0100

    Initial commit.

---
+++ js/new-age.js
@@ -0,0 +1,61 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 48)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 54
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  var options = {
+    strings: [
+        "an effective way to keep your records",
+        "a powerful tool to analyse your bank data",
+        "a simple interface to customise your views",
+        "the bank statement made easy",
+        "try it for free",
+    ],
+    typeSpeed: 50,
+    backSpeed: 20,
+    loop: true,
+    showCursor: false,
+    smartBackspace: false,
+    startDelay: 800,
+    backDelay: 2000,
+  }
+
+  var typed = new Typed(".subtitle", options);
+
+})(jQuery); // End of use strict
